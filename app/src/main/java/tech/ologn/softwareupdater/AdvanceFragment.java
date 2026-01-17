53781264b72eb8e2ac4fd2f9515ef34db6c379bf
Thanh Loc <loc.phan@ologn.tech>
2026-01-17 15:28:09 +0700

    Add configuration loading to spinner

--- app/src/main/java/tech/ologn/softwareupdater/AdvanceFragment.java
+++ app/src/main/java/tech/ologn/softwareupdater/AdvanceFragment.java
@@ -7,11 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.List;
 
 import tech.ologn.softwareupdater.utils.UpdateConfigs;
 
 public class AdvanceFragment extends Fragment {
+
+    private Spinner mSpinnerConfigs;
 
     public AdvanceFragment() {
         // Required empty public constructor
@@ -29,6 +35,10 @@ public class AdvanceFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         TextView mTextViewConfigsDirHint = view.findViewById(R.id.textViewConfigsDirHint);
         mTextViewConfigsDirHint.setText(UpdateConfigs.getConfigsRoot(requireContext()));
+
+        mSpinnerConfigs = view.findViewById(R.id.spinnerConfigs);
+
+        loadUpdateConfigs();
     }
 
     @Override
@@ -36,5 +46,24 @@ public class AdvanceFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_advance, container, false);
+    }
+
+    /**
+     * loads json configurations from configs dir that is defined in {@link UpdateConfigs}.
+     */
+    private void loadUpdateConfigs() {
+        List<UpdateConfig> mConfigs = UpdateConfigs.getUpdateConfigs(requireContext());
+        loadConfigsToSpinner(mConfigs);
+    }
+
+    private void loadConfigsToSpinner(List<UpdateConfig> configs) {
+        String[] spinnerArray = UpdateConfigs.configsToNames(configs);
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<>(requireContext(),
+                android.R.layout.simple_spinner_item,
+                spinnerArray);
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout
+                .simple_spinner_dropdown_item);
+        mSpinnerConfigs.setAdapter(spinnerArrayAdapter);
+
     }
 }
